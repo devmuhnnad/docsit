0eb815663f62d1398017dac00f49319492fa363c
devmuhnnad <muhnnadhabib@gmail.com>
2023-07-05 01:05:10 +0300

    Good point

---
+++ pages/docs/[doc].vue
@@ -0,0 +1,21 @@
+<script setup>
+const { params } = useRoute();
+
+// read file from docs folder
+
+const md = ref("");
+
+const getDoc = async () => {
+    const doc = await import("../../docs/intro.md");
+    console.log(doc);
+    md.value = doc.default;
+};
+
+
+getDoc();
+
+</script>
+
+<template>
+    {{ md }}
+</template>
