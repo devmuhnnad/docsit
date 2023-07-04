b685b52b91c92621679239eed2912d0fe5c631df
devmuhnnad <muhnnadhabib@gmail.com>
2023-07-04 23:30:17 +0300

    Header responsivity

--- components/Header.vue
+++ components/Header.vue
@@ -2,42 +2,76 @@
 <script setup>
 
 const config = useConfig()
-const iconName = useState("mdi:white-balance-sunny")
-const iconClass = useState("text-gray-900")
+const iconName = useState()
 
-watch(() => useColorMode().preference, (newVal) => {
-    if (newVal == "dark") {
+const menuOpen = ref(false)
+
+const toggleMenu = () => {
+    menuOpen.value = !menuOpen.value
+}
+
+const colorMode = useColorMode()
+
+
+watchEffect(() => {
+    if (colorMode.preference == "dark") {
         iconName.value = "ph:moon-fill"
-        iconClass.value = "text-gray-100"
     } else {
         iconName.value = "mdi:white-balance-sunny"
-        iconClass.value = "text-gray-900"
     }
 }, {
-    immediate: true
-
+    immediate: true,
 })
 
 </script>
 
 <template>
     <header class="text-gray-600 body-font">
-        <div class="w-full flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <NuxtLink to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 ">
+        <div class="w-full flex flex-wrap p-5 flex-row items-center gap-5">
+
+            <button class="text-gray-900 dark:text-gray-100 text-2xl md:hidden" @click="toggleMenu">
+                <Icon name="octicon:three-bars" />
+            </button>
+
+            <NuxtLink to="/" class="flex title-font font-medium items-center text-gray-900 ">
                 <nuxt-img :src="config.themeConfig.navbar.logo.src" :alt="config.themeConfig.navbar.logo.alt"
                     class="w-10 h-10 rounded-full" />
                 <span
                     class="ml-3 text-xl text-gray-900 dark:text-gray-100 hover:text-light-primary dark:hover:text-dark-primary">{{
                         config.title }}</span>
             </NuxtLink>
-            <nav class="md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <nav class="md:flex hidden flex-wrap items-center text-base justify-center">
                 <NuxtLink to="/docs"
                     class="ml-5 text-gray-900 dark:text-gray-100 hover:text-light-primary dark:hover:text-dark-primary font-bold">
                     Tutorial</NuxtLink>
 
             </nav>
-            <button @click="toggleTheme()" class="text-xl">
-                <Icon :name="iconName" :class="iconClass" size="1.5em" />
+            <div v-if="menuOpen" class="fixed w-screen h-screen bg-black top-0 left-0 opacity-50"></div>
+
+            <nav
+                :class="['fixed flex flex-col left-0 top-0 bg-white dark:bg-gray-900 p-5 w-9/12 h-screen ', !menuOpen && '-translate-x-full']">
+
+                <div class="flex justify-start items-center gap-5 border-b pb-2 z-10">
+                    <NuxtLink to="/" class="flex title-font font-medium items-center text-gray-900">
+                        <nuxt-img :src="config.themeConfig.navbar.logo.src" :alt="config.themeConfig.navbar.logo.alt"
+                            class="w-10 h-10 rounded-full" />
+                        <span
+                            class="ml-3 text-xl text-gray-900 dark:text-gray-100 hover:text-light-primary dark:hover:text-dark-primary">{{
+                                config.title }}</span>
+                    </NuxtLink>
+                    <button @click="toggleTheme()" class="text-xl">
+                        <Icon :name="iconName" class="text-gray-900 dark:text-gray-100" size="1em" />
+                    </button>
+
+                    <button class="ml-auto " @click="toggleMenu()">
+                        <Icon name="ic:outline-close" size="1.5em" />
+                    </button>
+                </div>
+
+            </nav>
+
+            <button @click="toggleTheme()" class="text-xl ml-auto hidden md:block">
+                <Icon :name="iconName" class="text-gray-900 dark:text-gray-100" size="1.5em" />
 
             </button>
         </div>
